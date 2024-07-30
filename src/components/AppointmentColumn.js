3fa6769694db3e7f045933e50107468e66128211
gangasaicharan <118888956+gangasaicharan@users.noreply.github.com>
2024-07-30 12:13:40 +0530

    Update AppointmentColumn.js

--- src/components/AppointmentColumn.js
+++ src/components/AppointmentColumn.js
@@ -3,9 +3,9 @@ import AppointmentCard from './AppointmentCard';
 
 function AppointmentColumn({type}) {
     const appointments = [
-        { id: 1, doctor: 'Dr. Bobby Sawyer', client: 'Mr. Shashi Singh', pet: 'Cat (Imli)', time: '9:30 - 10:00 AM' },
-        { id: 2, doctor: 'Dr. Kanishk', client: 'Mr. Sahil Singh', pet: 'Dog (Jerry)', time: '9:30 - 10:00 AM' },
-        { id: 3, doctor: 'Dr. Rahul Kumar', client: 'Mr. Shashi Singh', pet: 'Cat (Imli)', time: '9:30 - 10:00 AM' }
+        { id: 1, doctor: 'Dr. Bobby Sawyer', client: 'Mr. Shashi Singh', pet: 'Cat (Imli)', time: '9:30 - 10:00 AM', image:'download.jpg ' },
+        { id: 2, doctor: 'Dr. Kanishk', client: 'Mr. Sahil Singh', pet: 'Dog (Jerry)', time: '9:30 - 10:00 AM' ,image:'download.jpg ' },
+        { id: 3, doctor: 'Dr. Rahul Kumar', client: 'Mr. Shashi Singh', pet: 'Cat (Imli)', time: '9:30 - 10:00 AM',image:'download.jpg ' }
       ];
   return (
     
